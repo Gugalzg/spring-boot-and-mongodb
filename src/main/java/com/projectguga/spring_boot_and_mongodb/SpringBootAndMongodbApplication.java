efd2badc77a0b247de6ef6c0d6372f96e27ca884
Guga <luizgustavoguga15@gmail.com>
2024-10-11 10:12:46 -0300

    first commit

---
+++ src/main/java/com/projectguga/spring_boot_and_mongodb/SpringBootAndMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.projectguga.spring_boot_and_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectguga/spring_boot_and_mongodb/SpringBootAndMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectguga.spring_boot_and_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
